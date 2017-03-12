e0b7f30fe227809fb208c48877e8ebba5b41fbd1
Jonathan De Maeseneer <jldm42@gmail.com>
2017-03-11 19:23:48 -0800

    Job 11 - Role Specific maintenance list

--- application/controllers/Mtce.php
+++ application/controllers/Mtce.php
@@ -19,15 +19,20 @@ class Mtce extends Application {
 			foreach ($tasks as $task)
 			{
 				if (!empty($task->status))
-					$task->status = $this->statuses->get($task->status)->name;
-				$result .= $this->parser->parse('oneitem', (array) $task, true);
-			}
-			$this->data['display_tasks'] = $result;
+                                    $task->status = $this->statuses->get($task->status)->name;
+				//$result .= $this->parser->parse('oneitem', (array) $task, true);
 
-			// and then pass them on
-			$this->data['pagebody'] = 'itemlist';
-			$this->render();
-		}
+                                if($role == ROLE_OWNER)
+                                    $result .= $this->parser->parse('oneitemx', (array) $task, true);
+                                else
+                                    $result .= $this->parser->parse('oneitem', (array) $task, true);
+                                }
+                                $this->data['display_tasks'] = $result;
+
+                                // and then pass them on
+                                $this->data['pagebody'] = 'itemlist';
+                                $this->render();
+                        }
 		
 		// Extract & handle a page of items, defaulting to the beginning
 	function page($num = 1)
@@ -47,7 +52,14 @@ class Mtce extends Application {
 			if ($count >= $this->items_per_page) break;
 		}
 		$this->data['pagination'] = $this->pagenav($num);
-		$this->show_page($tasks);
+		
+                
+                $role = $this->session->userdata('userrole');
+                if($role == ROLE_OWNER)
+                $this->data['pagination'] .= $this->parser->parse('itemadd',[],true);
+                
+                
+                $this->show_page($tasks);
 	}
 	
 	// Build the pagination navbar
--- application/views/oneitem.php
+++ application/views/oneitem.php
@@ -1,5 +1,10 @@
 <tr>
-        <td>{id}</td>
+        <!-- 
+        We have a view fragment to show a single item in the task list, oneitem.
+        Let's make a comparable one to show for an owner, the only difference 
+        being that the ID should be shown as a link to Mtce::edit.
+        -->
+        <td><a href="/mtce/edit/{id}"><input type="button" value="{id}"/></a></td>
         <td>{task}</td>
         <td>{status}</td>
 </tr>
